2154ffcb74cda97a37fb893b11938e694246f340
paulo <paulorsantos.gnt@outlook.com>
2021-07-14 12:44:14 -0300

    Assignment 1 Ready - Airplane going forward - not automatic inclination - Camera positioned - propeller spinning

---
+++ Paulo_Santos_Assignment_1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initializes the offset position 
+        offset = new Vector3(37.0f,0.0f,3.0f);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Sets the camera position and update it frame by frame according to the plane movement
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Paulo_Santos_Assignment_1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float speed = 15.0f;
+    private float rotationSpeed = 100.0f;
+    private float verticalInput = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        // tilt the plane up/down based on up/down arrow keys
+        // Checks input values to prevent the plane from automatically tilting
+        if (verticalInput != 0)
+        {
+            transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime * verticalInput);
+        }
+    }
+}
