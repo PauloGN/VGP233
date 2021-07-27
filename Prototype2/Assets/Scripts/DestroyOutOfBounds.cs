f77f6b6866a3ab09a17c142002c29a23f79c5ab5
paulo <paulorsantos.gnt@outlook.com>
2021-07-26 22:03:09 -0300

    Prototype 2 ready

--- Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Prototype2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -7,12 +7,6 @@ public class DestroyOutOfBounds : MonoBehaviour
 
     private float verticalBound = 30.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
