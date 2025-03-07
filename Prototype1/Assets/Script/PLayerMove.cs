d7ef273d2c6e37905dfaef06183ba80d98347f16
PauloGN <paulorsantos.gnt@outlook.com>
2025-03-06 17:48:03 -0800

    Restarting My game life

---
+++ Prototype1/Assets/Script/PLayerMove.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PLayerMove : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * 20.0f);
+    }
+}
