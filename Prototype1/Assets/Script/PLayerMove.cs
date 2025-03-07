0469853dbe464bf52dc1624b1d5cb4ccfd8d7dbc
PauloGN <paulorsantos.gnt@outlook.com>
2025-03-06 18:08:43 -0800

    Change unity Version

--- Prototype1/Assets/Script/PLayerMove.cs
+++ Prototype1/Assets/Script/PLayerMove.cs
@@ -11,6 +11,6 @@ public class PLayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 20.0f);
+        transform.Translate(Vector3.forward * Time.deltaTime * 5.0f);
     }
 }
